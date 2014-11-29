ddce199255c669717598bf78cf2a06bf784d59f3
Brandon Hay <brandon.a.hay@gmail.com>
2014-11-29 10:58:45 -0500

    Removed the best-in-place js files

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -21,7 +21,7 @@ class RegistrationsController < Devise::RegistrationsController
       set_flash_message :notice, :updated
       # Sign in the user bypassing validation in case their password changed
       sign_in @user, :bypass => true
-      redirect_to root_path
+      redirect_to after_update_path_for(resource)
     else
       render "edit"
     end
@@ -43,7 +43,7 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-      params.require(:user).permit(:email, :password, :date_of_birth, :about, :avatar, :password_confirmation, :current_password)
+      params.require(:user).permit(:email, :password, :date_of_birth, :about, :avatar, :password_confirmation)
   end
 
 end
