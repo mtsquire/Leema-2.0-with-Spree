133e001579055a7e3ae04cabb137766abaa5a335
Brandon Hay <brandon.a.hay@gmail.com>
2014-12-15 19:58:35 -0500

    Re adding s3 stuff.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,15 +78,15 @@ Rails.application.configure do
   #Store image uploads in S3
 
 
-  # config.paperclip_defaults = {
-  #   :storage => :s3,
-  #   :s3_credentials => {
-  #     :bucket => ENV['S3_BUCKET_NAME'],
-  #     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  #     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  #   }
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    } 
 
-  #   url: ":s3_domain_url",
-  #   path: "/:class/:attachment/:id_partition/:style/:filename"
-  # }
+    url: ":s3_domain_url",
+    path: "/:class/:attachment/:id_partition/:style/:filename"
+  }
 end
