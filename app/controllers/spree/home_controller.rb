d8c87a304d5a0288ede63ffe89d52c93b9bf3f75
Brandon Hay <brandon.a.hay@gmail.com>
2015-01-06 00:29:45 -0500

    Added search functionality

--- app/controllers/spree/home_controller.rb
+++ app/controllers/spree/home_controller.rb
@@ -1,12 +1,18 @@
 module Spree
   class HomeController < Spree::StoreController
     helper 'spree/products'
-    respond_to :html
+    respond_to :html, :json
 
     def index
       @searcher = build_searcher(params.merge(include_images: true))
-      @products = @searcher.retrieve_products
       @taxonomies = Spree::Taxonomy.includes(root: :children)
+      @products = Spree::Product.search(params[:search])
+
+      respond_to do |format|
+        format.html # index.html.erb
+        format.json { render json: @products }
+      end
+
     end
   end
 end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,6 +1,12 @@
 Spree::Product.class_eval do
   belongs_to :user, class_name: Spree.user_class.to_s
-  def test_method
-    "hi"
+
+  def self.search(search)
+    if search
+      where(['name LIKE ?', "%#{search}%"])
+    else
+      @products = Spree::Product.all
+    end
   end
+
 end
