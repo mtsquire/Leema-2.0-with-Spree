3e593082fca7670990da4f4fabe1c0bc7df8dc9f
msquire <marc.squire@mlb.com>
2015-01-12 16:49:33 -0500

    ratings and reviews style updates

---
+++ app/overrides/add_reviews_after_product_info.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "spree/products/show",
+                     :name => "converted_product_properties_767643482",
+                     :insert_after => "#product-info-page",
+                     :partial => "spree/shared/reviews",
+                     :disabled => false)
