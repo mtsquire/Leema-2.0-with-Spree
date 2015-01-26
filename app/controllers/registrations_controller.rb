1cf28261698d29fbb3eb5cf47c37f1dc9ae63f00
Brandon Hay <brandon.a.hay@gmail.com>
2015-01-26 18:03:14 -0500

    trying to change redirect on user profile edit submission

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -21,7 +21,7 @@ class RegistrationsController < Devise::RegistrationsController
       set_flash_message :notice, :updated
       # Sign in the user bypassing validation in case their password changed
       sign_in @user, :bypass => true
-      redirect_to after_update_path_for(resource)
+      redirect_to '/#{current_user.id}'
     else
       render "edit"
     end
