40686f419cfbad7002550e352eacb7d667af41c8
Brandon Hay <brandon.a.hay@gmail.com>
2015-01-28 14:24:00 -0500

    Fixed sign in redirect loop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,7 @@ class ApplicationController < ActionController::Base
 
   layout :layout_by_resource
 
-  def after_sign_in_path_for(resource)
-    sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
-    if request.referer == sign_in_url
-      super
-    else
-      stored_location_for(resource) || request.referer || root_path
-    end
-  end
+  
 
   protected
     #Added so a user can edit their profile page.
