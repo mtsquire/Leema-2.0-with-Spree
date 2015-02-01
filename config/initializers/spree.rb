e08b11212308bbe92714d37b38fda8f9719fd4fb
Marc Squire <mtsquire@gmail.com>
2015-02-01 13:08:05 -0500

    adjusting image sizes for products and profile cover photo

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -23,10 +23,10 @@ Spree.config do |config|
         bucket:         ENV['S3_BUCKET_NAME'],
      
         styles: {
-          mini:     "48x48>",
-          small:    "100x100>",
-          product:  "240x240>",
-          large:    "600x600>"
+          mini:     "48x32>",
+          small:    "100x67>",
+          product:  "240x160>",
+          large:    "720x480>"
         },
      
         path:          ":rails_root/public/spree/products/:id/:style/:basename.:extension",
